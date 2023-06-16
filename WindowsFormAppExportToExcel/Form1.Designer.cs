ecd79b48f990a5e261bce9a7d3d151eb617b25f8
threepolesoft <threepolesoft@gmail.com>
2023-06-17 00:01:19 +0600

    Add project files.

---
+++ WindowsFormAppExportToExcel/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormAppExportToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 191);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(432, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(517, 190);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 390);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnExport;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormAppExportToExcel/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormAppExportToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<stdList> stdList = new List<stdList>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stdList.Add(new stdList
+            {
+                id = "181967001",
+                name = "Name 1",
+                roll = "001",
+                mob = "01758545548"
+            });
+
+            stdList.Add(new stdList
+            {
+                id = "181967002",
+                name = "Name 2",
+                roll = "002",
+                mob = "01758545548"
+            });
+
+            stdList.Add(new stdList
+            {
+                id = "181967003",
+                name = "Name 3",
+                roll = "003",
+                mob = "01758545548"
+            });
+
+            stdList.Add(new stdList
+            {
+                id = "181967004",
+                name = "Name 4",
+                roll = "004",
+                mob = "01758545548"
+            });
+
+            stdList.Add(new stdList
+            {
+                id = "181967005",
+                name = "Name 5",
+                roll = "005",
+                mob = "01758545548"
+            });
+
+            stdList.Add(new stdList
+            {
+                id = "181967006",
+                name = "Name 6",
+                roll = "006",
+                mob = "01758545548"
+            });
+
+            dataGridView1.DataSource = stdList.ToList();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                List<stdList> list = ((DataPara)e.Argument).stdList;
+                string fileName = ((DataPara)e.Argument).fileName;
+
+
+                Workbook wb = excel.Workbooks.Add(XlSheetType.xlWorksheet);
+                Worksheet ws = (Worksheet)excel.ActiveSheet;
+                excel.Visible = false;
+
+                int index = 1;
+                int procee = list.Count;
+
+
+                ws.Cells[1, 1] = "Id";
+                ws.Cells[1, 2] = "Name";
+                ws.Cells[1, 3] = "Roll";
+                ws.Cells[1, 4] = "Mob.";
+
+                foreach (stdList item in list)
+                {
+                    if (!backgroundWorker1.CancellationPending)
+                    {
+                        backgroundWorker1.ReportProgress(index++ * 100 / procee);
+
+                        ws.Cells[index, 1] = item.id;
+                        ws.Cells[index, 2] = item.name;
+                        ws.Cells[index, 3] = item.roll;
+                        ws.Cells[index, 4] = item.mob;
+
+
+                    }
+                }
+
+                //ws.SaveAs(fileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange,
+                //            XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+
+
+
+                ws.SaveAs(fileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, XlSaveAsAccessMode.xlExclusive,
+                    XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+
+
+                excel.Quit();
+
+                MessageBox.Show("Done");
+            }
+            catch (Exception)
+            {
+
+                excel.Quit();
+                MessageBox.Show("Fail");
+            }
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            progressBar1.Update();
+        }
+
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error==null)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        struct DataPara
+        {
+           public  List<stdList> stdList;
+            public string fileName { get; set; }
+        }
+
+        DataPara _inputDataPara;
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                return;
+            }
+            else
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel workbook|*.xlsx" })
+                {
+                    if (sfd.ShowDialog()==DialogResult.OK)
+                    {
+                        _inputDataPara.fileName = sfd.FileName;
+                        _inputDataPara.stdList = stdList.ToList();
+
+                        progressBar1.Minimum = 0;
+                        progressBar1.Value = 0;
+                        backgroundWorker1.RunWorkerAsync(_inputDataPara);
+                    }
+                }
+            }
+        }
+    }
+}
